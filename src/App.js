51daeacb90fe01dc3c3152b49c6cbf49a2eac9c2
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-03-27 15:32:35 +0100

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import './styles/App.css';
 import Header from 'components/Header';
 import TodosLogic from 'components/TodosLogic';
 
 const App = () => (
-  <>
-    <Header />
-    <TodosLogic />
-  </>
+  <div className="wrapper">
+    <div className="todos">
+      <Header />
+      <TodosLogic />
+    </div>
+  </div>
 );
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,19 @@
+import styles from 'styles/TodoItem.module.css';
 import PropTypes from 'prop-types';
 
 const TodoItem = (props) => {
   const { itemProp, handleChange, delTodo } = props;
   return (
-    <li>
-      <input
-        type="checkbox"
-        checked={itemProp.completed}
-        onChange={() => handleChange(itemProp.id)}
-      />
-      <button type="button" onClick={() => delTodo(itemProp.id)}>Delete</button>
-      {itemProp.title}
+    <li className={styles.item}>
+      <div className={styles.content}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <button type="button" onClick={() => delTodo(itemProp.id)}>Delete</button>
+        {itemProp.title}
+      </div>
 
     </li>
   );
