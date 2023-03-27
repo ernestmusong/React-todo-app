7cb4a41eed330ff96cac9920d0c5474fb8d47c09
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-03-27 23:06:46 +0100

    Add navbar with dropdown functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './styles/App.css';
 import Header from 'components/Header';
 import TodosLogic from 'components/TodosLogic';
+import Navbar from 'components/Navbar';
+import Modal from 'components/Modal';
 
 const App = () => (
-  <div className="wrapper">
-    <div className="todos">
-      <Header />
-      <TodosLogic />
+  <>
+    <Navbar />
+    <Modal />
+    <div className="wrapper">
+      <div className="todos">
+        <Header />
+        <TodosLogic />
+      </div>
     </div>
-  </div>
+  </>
 );
 export default App;
