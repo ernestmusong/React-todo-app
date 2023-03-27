4a987501bda5b41011c5a596e24f30121940a362
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-03-27 14:26:46 +0100

    Implement method to add a todo

---
+++ src/components/TodoItem.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+const TodoItem = (props) => {
+  const { itemProp, handleChange, delTodo } = props;
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={itemProp.completed}
+        onChange={() => handleChange(itemProp.id)}
+      />
+      <button type="button" onClick={() => delTodo(itemProp.id)}>Delete</button>
+      {itemProp.title}
+
+    </li>
+  );
+};
+
+TodoItem.propTypes = {
+  itemProp: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+};
+
+export default TodoItem;
